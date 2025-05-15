8060c8565f1b65dcaa8d675c659110edd54f980f
Darcy Mason <darcymason@gmail.com>
2025-05-15 17:19:03 -0400

    Working with flexelog and on_load to edit an entry

--- src/django_tuieditor/widgets.py
+++ src/django_tuieditor/widgets.py
@@ -1,35 +1,66 @@
 from django import forms
 
 
+common_css = {
+    "screen": (
+        "https://uicdn.toast.com/editor/latest/toastui-editor.min.css",
+        "https://uicdn.toast.com/chart/latest/toastui-chart.min.css",
+        "https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css",
+        "https://uicdn.toast.com/editor-plugin-color-syntax/latest/toastui-editor-plugin-color-syntax.min.css",
+        "https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight.min.css",
+        "https://uicdn.toast.com/editor-plugin-table-merged-cell/latest/toastui-editor-plugin-table-merged-cell.min.css",
+        "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css",  
+    ),  
+}
+
+
+common_js = (
+    "https://uicdn.toast.com/chart/latest/toastui-chart.min.js",
+    "https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.js",
+    "https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js",
+    "https://uicdn.toast.com/editor-plugin-chart/latest/toastui-editor-plugin-chart.min.js",
+    "https://uicdn.toast.com/editor-plugin-code-syntax-highlight/latest/toastui-editor-plugin-code-syntax-highlight-all.min.js",
+    "https://uicdn.toast.com/editor-plugin-color-syntax/latest/toastui-editor-plugin-color-syntax.min.js",
+    "https://uicdn.toast.com/editor-plugin-table-merged-cell/latest/toastui-editor-plugin-table-merged-cell.min.js",
+    "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js",    
+)
 class MarkdownEditorWidget(forms.Textarea):
     template_name = 'django_tuieditor/editor.html'
 
     class Media:
-        css = { 'screen': ( 'django_tuieditor/codemirror.css', 'django_tuieditor/toastui-editor.css', 'django_tuieditor/django-fixes.css', ) }
-        js = ( 'django_tuieditor/codemirror.js', 'django_tuieditor/toastui-editor.js', 'django_tuieditor/django-fixes.js', )
+        css = common_css
+        js = common_js
+
+    def __init__(self, attrs=None):
+        default_attrs = {'hidden': True}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(default_attrs)
+
 
 class MarkdownViewerWidget(forms.Textarea):
     template_name = 'django_tuieditor/viewer.html'
 
     def __init__(self, attrs=None):
-        default_attrs = {'class': 'tui-editor-contents'}
+        default_attrs = {'class': 'tui-editor-contents', 'hidden': True}
         if attrs:
             default_attrs.update(attrs)
         super().__init__(default_attrs)
 
     class Media:
-        css = { 'screen': ( 'django_tuieditor/codemirror.css', 'django_tuieditor/toastui-editor.css', 'django_tuieditor/django-fixes.css', ) }
-        js = ( 'django_tuieditor/codemirror.js', 'django_tuieditor/toastui-editor-viewer.js', 'django_tuieditor/django-fixes.js', )
+        css = common_css
+        js = common_js
 
-class StaticMarkdownViewerWidget(forms.Textarea):
-    template_name = 'django_tuieditor/static_viewer.html'
+# class StaticMarkdownViewerWidget(forms.Textarea):
+#     template_name = 'django_tuieditor/static_viewer.html'
 
-    def __init__(self, attrs=None):
-        default_attrs = {'class': 'tui-editor-contents'}
-        if attrs:
-            default_attrs.update(attrs)
-        super().__init__(default_attrs)
+#     def __init__(self, attrs=None):
+#         default_attrs = {'class': 'tui-editor-contents'}
+#         if attrs:
+#             default_attrs.update(attrs)
+#         super().__init__(default_attrs)
 
-    class Media:
-        css = { 'screen': ( 'django_tuieditor/toastui-editor.css', 'django_tuieditor/django-fixes.css', ) }
-        js = ()
+#     class Media:
+#         css = { 'screen': ( 'django_tuieditor/toastui-editor.css', 'django_tuieditor/django-fixes.css', ) }
+#         js = ()
